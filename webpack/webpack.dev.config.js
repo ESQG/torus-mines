2fcba0b9d96111d6d4146e64374db0e1629ba790
Elizabeth Goodman <esqg@alumni.stanford.edu>
2018-10-13 18:10:32 -0700

    webpack setup

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: [
+		path.join(__dirname, '../src/index.js')
+  ],
+	module: {
+		loaders: [{
+			test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: __dirname + '/build',
+    historyApiFallback: true
+  }
+}
