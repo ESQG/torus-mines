b41713753a491ddf116eb3a8bda6bca28bb66fc0
Elizabeth Goodman <esqg@alumni.stanford.edu>
2019-01-26 16:22:14 -0800

    rendering tests with inline styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
 
 import React, {Component} from 'react';
 
+const N_MINES = 20;
+const BOARD_WIDTH = 12;
+const BOARD_HEIGHT = 10;
+
 export default class App extends Component {
     render () {
-        return <p>Rendered by React if working</p>
+        return <p>Rendered by React if working. <br />
+			<Game />
+        </p>
     }
+}
+
+class SquareInfo {
+
+}
+
+class Game extends Component {
+
+	constructor() {
+		super();
+		const boardInfo = [];
+		for(let i=0; i<BOARD_HEIGHT; i++) {
+			let row = [];
+			for (let j=0; j<BOARD_WIDTH; j++) {
+				row.push({
+					isMine: false,
+					mineState: MineState.Revealed,
+					numNeighbors: i
+				});
+			}
+			boardInfo.push(row);
+		}
+		this.state = { boardInfo: boardInfo };
+	}
+
+	renderRow(row, i) {
+		const tds = row.map((cell, j) => <td>{i*j}</td>);
+		return <tr>{tds}</tr>;
+	}
+
+	render() {
+		return <table className="board">
+			{this.state.boardInfo.map(this.renderRow)}
+		</table>;
+	}
+
+
+}
+
+const MineState = {
+	Secret: 0,
+	Revealed: 1,
+	Flagged: 2,
+};
+
+class Square extends Component {
+
+	render() {
+		const { mineState, numNeighbors, isMine } = this.props;
+		let buttonContents = null;
+		if (mineState == MineState.Flagged) {
+			buttonContents = "!";
+		}
+		else if (mineState == MineState.Revealed) {
+			if (isMine) {
+				buttonContents = "X";
+			}
+			else {
+				buttonContents = numNeighbors;
+			}
+		}
+		return <button className="square">{buttonContents}</button>;
+	}
 }
